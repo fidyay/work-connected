39beb58e88c1f37d1b13441f1b5acd4b5658fb65
Fidyay <fidiy1878@gmail.com>
2023-07-26 23:43:07 +0300

    Added animations for selection roles

--- src/components/Role.tsx
+++ src/components/Role.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Cross from "./svg/Cross";
 import styles from "@/styles/role.module.scss";
-
+import { CSSTransition } from "react-transition-group";
 interface RoleProps {
   children: string;
   onClick?: React.MouseEventHandler<HTMLSpanElement>;
@@ -10,13 +10,29 @@ interface RoleProps {
 }
 
 function Role({ children, onClick, selected }: RoleProps) {
+  const cross = useRef<HTMLSpanElement>(null);
+
   return (
     <span
       className={styles.span + (selected ? ` ${styles.selected}` : "")}
       onClick={onClick}
     >
       {"@" + children}
-      {!!selected && <Cross className={styles.cross} />}
+      <CSSTransition
+        in={selected}
+        nodeRef={cross}
+        timeout={200}
+        mountOnEnter
+        unmountOnExit
+        classNames={{
+          enterActive: styles.cross_enter_active,
+          enterDone: styles.cross_enter_active,
+          exitActive: styles.cross_exit_active,
+          exitDone: styles.cross_exit_active,
+        }}
+      >
+        <Cross className={styles.cross_default} ref={cross} />
+      </CSSTransition>
     </span>
   );
 }
--- src/components/svg/Cross.tsx
+++ src/components/svg/Cross.tsx
@@ -1,28 +1,32 @@
-import React from "react";
-
+import React, { forwardRef } from "react";
+import styles from "@/styles/cross.module.scss";
 interface CrossProps {
   style?: React.CSSProperties;
   className?: string;
 }
 
-function Cross({ style, className = "" }: CrossProps) {
+const Cross = forwardRef(function Cross(
+  { style, className = "" }: CrossProps,
+  ref?: React.ForwardedRef<HTMLSpanElement>
+) {
   return (
-    <svg
-      style={style}
-      className={className}
-      width="10"
-      height="10"
-      viewBox="0 0 10 10"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M1.464 8.53503L8.536 1.46503M1.464 1.46503L8.536 8.53503"
-        stroke-width="1.5"
-        stroke-linecap="round"
-      />
-    </svg>
+    <span ref={ref} style={style} className={className}>
+      <svg
+        className={styles.cross}
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.464 8.53503L8.536 1.46503M1.464 1.46503L8.536 8.53503"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+      </svg>
+    </span>
   );
-}
+});
 
 export default Cross;
